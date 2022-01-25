b053a097af86bb8fead3daf521d5c3d7654520c8
Camille Barbit <barbitcamille@gmail.com>
2022-01-25 18:13:12 +0100

    update?

--- server.c
+++ server.c
@@ -77,40 +77,6 @@ void    handle_signals(int signum, siginfo_t *info, void *useless)
     i++;
 }
 
-// void    ft_handle_signals(int signum, siginfo_t *info, void *useless)
-// {
-//     static int  i;
-//     static int  j;
-//     static char str[100000];
-//     (void)info; //je n'utilise pas ce parametre
-//     (void)useless; //je n'utilise pas ce parametre
-
-//     if (i = 0)
-//         str[j] = '\0';
-//     if (i < 8)
-//     {
-//         str[j] = str[j] << 1;
-//         if (signum == SIGUSR2)
-//             str[j] = str[j] + 1;
-//         if (signum == SIGUSR1)
-//             str[j] = str[j] + 0;
-//         i++;
-//     }
-//     if (i == 7)
-//     {
-//         if (!str[j] || j = 99999)
-//         {
-//             write(1, str, j);
-//             str[0] = '\0';
-//             i = 0;
-//             j = 0;
-//             return ;
-//         }
-//         i = 0;
-//         j++;
-//     }
-// }
-
 int main(void)
 {
     struct sigaction    sa;
