32e349be5eb45d416dd729f49a9573aa0bd285fa
Camille BARBIT <cbarbit@e1r12p13.42.fr>
2022-01-26 16:43:05 +0100

    new version

--- client.h
+++ client.h
@@ -6,19 +6,22 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 11:18:42 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/01/26 16:21:39 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/01/26 16:27:04 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include <signal.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <signal.h>
-#include <stdio.h>
+# include <signal.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <ctype.h>
+# include <signal.h>
+# include <stdio.h>
+
+static int	ft_strlen(char *str);
+int 		ft_atoi(const char *str);
 
 #endif
