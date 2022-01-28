aac6ecc2018eb05162f49370e8c2fbb450b2380b
CamilleBarbit <85878035+CamilleBarbit@users.noreply.github.com>
2022-01-28 15:23:17 +0100

    Add files via upload

---
+++ client_srcs/client.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 11:18:42 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/01/28 14:48:44 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+
+# include <signal.h>
+# include <limits.h>
+# include "../ft_printf/ft_printf.h"
+
+int		ft_strlen(char *str);
+int		ft_atoi(const char *str);
+void	char_to_binary(char c, pid_t pid);
+void	int_to_binary(int c, pid_t pid);
+void	ft_check_parameters(int argc, char **argv);
+
+#endif
---
+++ client_srcs/client_utils.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 15:17:37 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/01/28 12:13:32 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "client.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_atoi(const char *str)
+{
+    int	i;
+    int	nbr;
+    int	sign;
+
+    i = 0;
+    nbr = 0;
+    sign = 1;
+    while (str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r'
+		|| str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nbr = nbr * 10 + str[i] - '0';
+		i++;
+	}
+	return (nbr * sign);
+}
